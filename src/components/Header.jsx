00bb41ddfe3432833877b30205c258f98e42ffc2
ID22663 <gayratyoldoshboyev3@gmail.com>
2024-07-14 10:31:24 +0500

    home page

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    const link = ['Home', 'About us', 'Features', 'FAQg']
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
