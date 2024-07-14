3fffb1d4660d2c7a374d079f775968359a5622ae
ID22663 <gayratyoldoshboyev3@gmail.com>
2024-07-14 10:43:46 +0500

    new pages add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
 import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header'
+import Home from './Home';
+import About from './About';
+import Features from './Features';
+import Pricing from './Pricing';
+import FAQ from './FAQ';
+import Blogs from './Blogs';
 
 function App() {
-
-
   return (
-    <div>
-
-    </div>
-  )
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/features" component={Features} />
+          <Route path="/pricing" component={Pricing} />
+          <Route path="/faq" component={FAQ} />
+          <Route path="/blogs" component={Blogs} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+// Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-    const link = ['Home', 'About us', 'Features', 'FAQg']
-  return (
-    <div>
-        <nav>
-            <ul>
-                <li></li>
-            </ul>
-        </nav>
-    </div>
-  )
+    const links = [
+        { name: 'Home', path: '/' },
+        { name: 'About us', path: '/about' },
+        { name: 'Features', path: '/features' },
+        { name: 'Pricing', path: '/pricing' },
+        { name: 'FAQ', path: '/faq' },
+        { name: 'Blogs', path: '/blogs' }
+    ];
+    return (
+        <div>
+            <nav>
+                <ul>
+                    {links.map((link, index) => (
+                        <li key={index}>
+                            <Link to={link.path}>{link.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
